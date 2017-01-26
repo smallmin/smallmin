9dcbec96054332ebfd9a1c8f28705591c7dac261
smallmin <1021206214@qq.com>
2017-01-26 14:25:51 +0800

    采用Maven&SpringBoot框架，初步搭建

---
+++ src/main/java/com/smallmin/App.java
@@ -0,0 +1,17 @@
+package com.smallmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/smallmin/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.smallmin;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+/** 
+* @author jmlu
+* @version 2017年1月25日 下午4:48:31 
+*  
+*/
+@Configuration
+public class FilterConfig {
+	
+	@Bean
+	public FilterRegistrationBean encodingFilter() {
+		//set encoding to UTF-8
+	    FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+	    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+	    characterEncodingFilter.setForceEncoding(true);
+	    characterEncodingFilter.setEncoding("UTF-8");
+	    registrationBean.setFilter(characterEncodingFilter);
+	    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	    return registrationBean;
+	}
+}
